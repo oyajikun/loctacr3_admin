e7a80b3e4b94a2ac365c7f82944cbb963bab961f
oyajikun <oyajikun>
2011-01-09 10:34:55 +0900

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loctacr3Admin::Application.config.secret_token = '06444743b14218ad11615b268a11f29e6c8e71de5f32f62e6660ab350b7ca7e5af657a4607d742c83c8efa969456ce44c5a3e2eb926394969b099f1a2c61c591'
